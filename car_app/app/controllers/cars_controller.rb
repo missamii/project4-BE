82b0d83bf17b4bc1169120c4ee023565da91d53c
carrow thibault <carrow1007@gmail.com>
2016-08-29 13:28:54 -0400

    edits

--- car_app/app/controllers/cars_controller.rb
+++ car_app/app/controllers/cars_controller.rb
@@ -1,13 +1,16 @@
 class CarsController < ApplicationController
     def trigger_cars_api
 
-    char_name = params[:name]
+    car_name = params[:name]
 
     puts "CAR_API_KEY:", ENV["CAR_API_KEY"]
 
     cars_api_key = ENV["CAR_API_KEY"]
 
-    fullQuery = "http://api.edmunds.com/api/vehicle/v2/makes?fmt=json&year=1995&api_key=#{cars_api_key}"
+    fullQuery = "http://api.edmunds.com/api/vehicle/v2/#{car_name}?fmt=json&year=2016&api_key=#{cars_api_key}"
+
+
+    puts "fullQuery:", [fullQuery]
 
     data = HTTParty.get(fullQuery)
     render json: data
