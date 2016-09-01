5494a51209ed7c1cb135d77cae3df8c9624724ab
carrow thibault <carrow1007@gmail.com>
2016-09-01 19:53:31 -0400

    adding

--- db/migrate/20160829201517_create_messages.rb
+++ db/migrate/20160829201517_create_messages.rb
@@ -4,7 +4,6 @@ class CreateMessages < ActiveRecord::Migration[5.0]
       t.string :name
       t.string :email
       t.string :message
-      t.attachment :image
       t.timestamps
     end
   end
